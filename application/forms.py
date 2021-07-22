7fdf87ff72cf91d05b136a4474766f36a5c63de2
MariusCSaunders <mchastinsaunders@gmail.com>
2021-07-22 13:42:58 +0000

    MS: Updated files for update feature

--- application/forms.py
+++ application/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, BooleanField
+from wtforms import StringField, SubmitField, IntegerField
 
 class FrankForm(FlaskForm):
-    title = StringField("What is name of the setup?")
-    description = StringField("What is the description of your setup")
+    title = StringField("What is title of the setup?")
+    description = StringField("What is the description of your setup?")
     tama = StringField("What type of tama?")
     sarado = StringField("What type of sarado?")
-    sword = StringField("What type of sword")
+    sword = StringField("What type of sword?")
     string = IntegerField("How long is the string in CM?")
-    bearing = BooleanField("Does the setup have a bearing?")
+    bearing = StringField("Does the setup have a bearing?")
     submit = SubmitField("Submit")
--- application/models.py
+++ application/models.py
@@ -8,11 +8,12 @@ class Frankendama(db.Model):
     sarado = db.Column(db.String(25), nullable=False)
     sword = db.Column(db.String(25), nullable=False)
     string = db.Column(db.String(25), nullable=False)
-    bearing = db.Column(db.Boolean, nullable=False, default=True)
+    bearing = db.Column(db.String(5), nullable=False)
     #company_id = db.Column(db.Integer, db.foreignKey('company_id'), nullable=False)
+    companies = db.relationship('Company', backref='frankendama', lazy=True)
 
 
-#class Company(db.Model):
-#    company_id = db.Column(db.Integer, primary_key=True)
-#    name = db.Column(db.String(25), nullable=False)
-#    frankendama = db.relationship('Frankendama', backref='company')
+class Company(db.Model):
+    company_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    frankendama_id = db.Column(db.Integer, db.ForeignKey('Frankendama'), nullable=False)
