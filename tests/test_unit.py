b3f1c25150206ee4437d2bf12542d528365adc6e
MariusCSaunders <mchastinsaunders@gmail.com>
2021-07-23 13:35:38 +0000

    MS:Half functioning unit tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,7 @@
 from flask_testing import TestCase
+from flask_sqlalchemy import SQLAlchemy
 from flask import url_for
+import os
 
 from application import app, db
 from application.models import Company, Frankendama
@@ -7,43 +9,55 @@ from application.models import Company, Frankendama
 
 class TestBase(TestCase):
 
+    # Pass in testing configurations for the app. Here we use sqlite without a persistent database for our tests.
     def create_app(self):
 
         app.config.update(
-            SQLALCHEMY_DATABASE_URI= "sqlite:///data.db"
+            SQLALCHEMY_DATABASE_URI="sqlite:///data.db",
+            SECRET_KEY='TEST_SECRET_KEY',
+            DEBUG=True,
+            WTF_CSRF_ENABLED=False
         )
 
         return app
-
     
+    #Testing method for setting up the test database
     def set_up(self):
 
+        # Create table
         db.create_all()
 
+        # Create test frankendama
         frank1 = Frankendama(
             title="Taps",
             description="A combo of damas designed for taps",
-            tama="SK x Ceral STIK",
+            tama="SK x Cereal STIK",
             sarado="Lomond Shape",
             sword="Lomond Shape",
             string="72",
             bearing="Yes"
         )
 
+        # Create test companies
         company1 = Company(name = "CEREAL", frankendama_id = 1)
         company2 = Company(name = "SK", frankendama_id = 1)
 
+        # Save Database entries 
         db.session.add(frank1)
         db.session.add(company1)
         db.session.add(company2)
 
         db.session.commit()
 
-    
+
+    #Testing method to destroy the testing database
     def tear_down(self):
 
+        db.session.remove()
         db.drop_all()
 
+
+#Testing method to test the loading of the three main pages works.
 class TestViews(TestBase):
 
     def test_home(self):
@@ -61,9 +75,13 @@ class TestViews(TestBase):
 class TestRead(TestBase):
 
     def test_home(self):
-        response = self.client.get(url_for('home'))
+        response = self.client.get(
+            url_for('home'),
+            follow_redirects= True
+            )
+
         assert "Taps" in response.data.decode()
-        assert "A combo of damas designed for taps" in response.data.decode()
+        assert "Check updated task" in response.data.decode()
         assert "SK x Ceral STIK" in response.data.decode()
         assert "Lomond Shape" in response.data.decode()
         assert "Lomond Shape" in response.data.decode()
